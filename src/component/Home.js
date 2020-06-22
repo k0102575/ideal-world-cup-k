e74a0d2ee1eb8f26a5a92e950b7757e70cc64abb
SeungMin <k0102575@gmail.com>
2020-06-22 23:31:53 +0900

    get json data

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { Fragment, useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
-function Home() {
-  return <div>Home</div>;
-}
+const Home = () => {
+  const [worldCups, setWorldCups] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    fetch('/test.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setWorldCups(data.worldCups);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
+  return (
+    <Fragment>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        worldCups.map((e, index) => {
+          return (
+            <div key={index}>
+              <p>
+                <Link to={`/world-cup/${e.id}`}>{e.id}</Link>
+              </p>
+              <p>{e.name}</p>
+            </div>
+          );
+        })
+      )}
+    </Fragment>
+  );
+};
 
 export default Home;
