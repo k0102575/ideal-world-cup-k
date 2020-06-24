d03378b47584ffd5961c6d8a9dd738b7da459b2b
SeungMin <k0102575@gmail.com>
2020-06-24 22:48:18 +0900

    not found history go home

--- src/component/NotFound.js
+++ src/component/NotFound.js
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 
 const NotFound = () => {
   // history
   const history = useHistory();
+
+  // useEffect
+  useEffect(() => {
+    history.push('/');
+  }, [history]);
 
   return <div>Not Found</div>;
 };
